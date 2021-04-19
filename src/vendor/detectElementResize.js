cc0dbd9fc324ed01501d5ccdde5c9345d2452d59
Ari Kramer <ari.kramer@beacon.io>
2021-04-19 18:08:23 -0400

    fix(resetTriggers): add 1 to expand scrollLeft/scrollTop
    This ensures the AutoSizer will render properly even when browser zoom is <= 80%

--- src/vendor/detectElementResize.js
+++ src/vendor/detectElementResize.js
@@ -24,31 +24,31 @@ export default function createDetectElementResize(nonce) {
   var attachEvent = typeof document !== 'undefined' && document.attachEvent;
 
   if (!attachEvent) {
-    var requestFrame = (function() {
+    var requestFrame = (function () {
       var raf =
         _window.requestAnimationFrame ||
         _window.mozRequestAnimationFrame ||
         _window.webkitRequestAnimationFrame ||
-        function(fn) {
+        function (fn) {
           return _window.setTimeout(fn, 20);
         };
-      return function(fn) {
+      return function (fn) {
         return raf(fn);
       };
     })();
 
-    var cancelFrame = (function() {
+    var cancelFrame = (function () {
       var cancel =
         _window.cancelAnimationFrame ||
         _window.mozCancelAnimationFrame ||
         _window.webkitCancelAnimationFrame ||
         _window.clearTimeout;
-      return function(id) {
+      return function (id) {
         return cancel(id);
       };
     })();
 
-    var resetTriggers = function(element) {
+    var resetTriggers = function (element) {
       var triggers = element.__resizeTriggers__,
         expand = triggers.firstElementChild,
         contract = triggers.lastElementChild,
@@ -57,18 +57,18 @@ export default function createDetectElementResize(nonce) {
       contract.scrollTop = contract.scrollHeight;
       expandChild.style.width = expand.offsetWidth + 1 + 'px';
       expandChild.style.height = expand.offsetHeight + 1 + 'px';
-      expand.scrollLeft = expand.scrollWidth;
-      expand.scrollTop = expand.scrollHeight;
+      expand.scrollLeft = expand.scrollWidth + 1;
+      expand.scrollTop = expand.scrollHeight + 1;
     };
 
-    var checkTriggers = function(element) {
+    var checkTriggers = function (element) {
       return (
         element.offsetWidth != element.__resizeLast__.width ||
         element.offsetHeight != element.__resizeLast__.height
       );
     };
 
-    var scrollListener = function(e) {
+    var scrollListener = function (e) {
       // Don't measure (which forces) reflow for scrolls that happen inside of children!
       if (
         e.target.className &&
@@ -84,11 +84,11 @@ export default function createDetectElementResize(nonce) {
       if (this.__resizeRAF__) {
         cancelFrame(this.__resizeRAF__);
       }
-      this.__resizeRAF__ = requestFrame(function() {
+      this.__resizeRAF__ = requestFrame(function () {
         if (checkTriggers(element)) {
           element.__resizeLast__.width = element.offsetWidth;
           element.__resizeLast__.height = element.offsetHeight;
-          element.__resizeListeners__.forEach(function(fn) {
+          element.__resizeListeners__.forEach(function (fn) {
             fn.call(element, e);
           });
         }
@@ -134,15 +134,15 @@ export default function createDetectElementResize(nonce) {
       keyframeprefix + 'animation: 1ms ' + animationName + '; ';
   }
 
-  var createStyles = function(doc) {
+  var createStyles = function (doc) {
     if (!doc.getElementById('detectElementResize')) {
       //opacity:0 works around a chrome bug https://code.google.com/p/chromium/issues/detail?id=286360
       var css =
-          (animationKeyframes ? animationKeyframes : '') +
-          '.resize-triggers { ' +
-          (animationStyle ? animationStyle : '') +
-          'visibility: hidden; opacity: 0; } ' +
-          '.resize-triggers, .resize-triggers > div, .contract-trigger:before { content: " "; display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; z-index: -1; } .resize-triggers > div { background: #eee; overflow: auto; } .contract-trigger:before { width: 200%; height: 200%; }',
+        (animationKeyframes ? animationKeyframes : '') +
+        '.resize-triggers { ' +
+        (animationStyle ? animationStyle : '') +
+        'visibility: hidden; opacity: 0; } ' +
+        '.resize-triggers, .resize-triggers > div, .contract-trigger:before { content: " "; display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; z-index: -1; } .resize-triggers > div { background: #eee; overflow: auto; } .contract-trigger:before { width: 200%; height: 200%; }',
         head = doc.head || doc.getElementsByTagName('head')[0],
         style = doc.createElement('style');
 
@@ -163,7 +163,7 @@ export default function createDetectElementResize(nonce) {
     }
   };
 
-  var addResizeListener = function(element, fn) {
+  var addResizeListener = function (element, fn) {
     if (attachEvent) {
       element.attachEvent('onresize', fn);
     } else {
@@ -208,7 +208,7 @@ export default function createDetectElementResize(nonce) {
     }
   };
 
-  var removeResizeListener = function(element, fn) {
+  var removeResizeListener = function (element, fn) {
     if (attachEvent) {
       element.detachEvent('onresize', fn);
     } else {
